5b931a237c4a894d2ac1a06ef407c193bc3ad72d
Anton Malkov <anton.malkov@gmail.com>
2015-11-21 21:16:03 +0100

    Turn this into a one-liner.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,5 @@
-(1..100).each do |i|
-  str = (i % 3 == 0 ? "Fizz" : "") +
-        (i % 5 == 0 ? "Buzz" : "")
-  puts str.empty? ? i.to_s : str
-end
+puts ((1..100).to_a.map do |i|
+        (i % 3 == 0 ? 'Fizz' : '') +
+        (i % 5 == 0 ? 'Buzz' : '') +
+        (i % 3 != 0 && i % 5 != 0 ? i.to_s : '')
+      end).join(' ')
